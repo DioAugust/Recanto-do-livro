6c4de7bff33f5b10f8a092da39eeb8284a89c545
GabrielSilvaDS <gabrielsilvadomingues@hotmail.com>
2022-08-16 20:46:02 -0300

    Acrescentendo novos botões para retroceder

--- server/app.js
+++ server/app.js
@@ -18,6 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../client/public')));
+app.use(express.static(__dirname + '/public'));
 
 
 app.use(session({
